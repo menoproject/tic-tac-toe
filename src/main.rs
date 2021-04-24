c6561e64f6ed2f941fe787788bd647cb6833a0ae
Charles Costello <charlescostello@MacBook-Pro.local>
2021-04-24 12:39:51 -0600

    board size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let board_size: i32 = std::env::args()
+        .nth(1).expect("Enter board size.")
+        .parse().expect("Board size must be an int.");
+    println!("{}", board_size);
 }
