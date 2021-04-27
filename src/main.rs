6c203d38529e369e0a67266c57a0dd29abb49e45
Charles Costello <charlescostello@MacBook-Pro.local>
2021-04-26 21:33:47 -0600

    main loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use std::io::stdin;
+
+struct Board {
+    size: i32
+}
+
+
 fn main() {
-    let board_size: i32 = std::env::args()
+    let size: i32 = std::env::args()
         .nth(1).expect("Enter board size.")
         .parse().expect("Board size must be an int.");
-    println!("{}", board_size);
+    
+    let board = Board { size };
+
+    for elem in 0..10 {
+        let mut player_move = String::new();
+        stdin().read_line(&mut player_move).expect("Please enter a move");
+
+        println!("{}", player_move);
+    }
+
+    println!("{}", board.size);
 }
